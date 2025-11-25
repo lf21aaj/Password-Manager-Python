05486bd60f0fb106afee7e2d3c98acd5aef87edc
lf21aaj <lf21aaj@herts.ac.uk>
2025-11-25 11:25:10 +0000

    Added password encryption, add load_key and write_key, creates key.key file.

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,25 @@
-master_pwd = input("Please enter the master password: ")
+# encryption module
+from cryptography.fernet import Fernet
+
+# only needs calling once to create key
+'''
+def write_key():
+    key = Fernet.generate_key()
+    # write bytes mode 'wb'
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    # read bytes mode 'rb'
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+# initialising encryption module
+fer = Fernet(key)
 
 def view():
     with open("passwords.txt", "r") as file:
@@ -6,7 +27,7 @@ def view():
             data = line.rstrip()
             # split string between account name and password
             user, passw = data.split("|")
-            print(f"Account Name: {user} | Password: {passw}")
+            print(f"Account Name: {user} | Password: {fer.decrypt(passw.encode()).decode()}")
 
 def add():
     name = input("\nAccount Name: ")
@@ -14,9 +35,8 @@ def add():
 
     # auto closes file
     with open("passwords.txt", "a") as file:
-        file.write(f"{name} | {pwd}\n")
-
-
+        # encode pwd to turn to byte str, then decode to regular str
+        file.write(f"{name} | {fer.encrypt(pwd.encode()).decode()}\n")
 
 while True:
     mode = input("\nWould you like to add a new password, or view existing ones? (add, view), press q to quit: ").lower()
